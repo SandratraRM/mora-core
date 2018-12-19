8c60e45d0ebd8401227334d9167fbbc1fc7265e7
SandratraRM <sandratrarambelosonm@gmail.com>
2018-12-19 16:04:56 +0300

    Validate

---
+++ src/cli/Manager/Controller/ControllerDelete.php
@@ -0,0 +1,13 @@
+<?php
+namespace Mora\Core\cli\Manager\Controller;
+class ControllerDelete{
+    public static function delete($name){
+        
+    }
+    private static function deleteRoute($name){
+        
+    }
+    private static function deleteFirewallTarget(){
+        
+    }
+}
---
+++ src/cli/Manager/Controller/ControllerMessage.php
@@ -0,0 +1,17 @@
+<?php
+namespace Mora\Core\cli\Manager\Controller;
+class ControllerMessage{
+    public static function create_success($name,$actions){
+        
+    }
+    public static function delete_success($name){
+        
+    }
+    public static function rename_success($old,$new){
+        
+    }
+    public static function add_actions_success($actions){
+        
+    }
+    
+}
