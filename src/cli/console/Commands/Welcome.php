d6740f8c4ec3c41d4958f2e825ba4f8d0c54a673
SandratraRM <sandratrarambelosonm@gmail.com>
2018-12-25 16:41:11 +0300

    Command welcome

--- src/cli/console/Commands/Welcome.php
+++ src/cli/console/Commands/Welcome.php
@@ -5,6 +5,7 @@ use Mora\Core\cli\Console\Output;
 use Mora\Core\cli\Console\CliConfig;
 use Mora\Core\cli\Console\CliStrings;
 use Mora\Core\cli\Helpers\SkeletonLoader;
+use Mora\Core\Config\JsonConfigManager;
 
 class Welcome{
     public static function firstTime()
@@ -23,13 +24,24 @@ class Welcome{
     private static function WelcomeMessage(){
         $welcome = "\r\n<yellow>".CliStrings::get("welcome")."<nc>\r\n";
         $usage = "\r\n<yellow>".CliStrings::get("usage")."<nc>\r\n";
-        $command = "    php mora <blue><".CliStrings::get("command")."> <green>[".CliStrings::get("subcommand")."]<nc>\r\n\r\n";
-        Output::print($welcome,self::getLogo(),$usage,$command);
+        $command = "    php mora \033[1;4;34m".CliStrings::get("command")."<nc> <green>[".CliStrings::get("subcommand")." <cyan>args<green>]<nc>\r\n";
+        $tip = Output::style(CliStrings::get("usage_tip"))."\r\n"; 
+        Output::print($welcome,self::getLogo(),$usage,$command,$tip);
     }
     private static function printCommandList(){
+        ob_start();
         Output::printWarning(CliStrings::get("command_list"));
-        Output::print(CliStrings::replace(
-            SkeletonLoader::get("commandlist")
-        ));
+        $path = __DIR__ . "/CommandList.json";
+        $conf = new JsonConfigManager($path);
+        $commands = $conf->getConfigsArray();
+        foreach ($commands as $command => $subcommands) {
+            Output::print("\r\n\033[1;4;34m$command<nc>","\r\n");
+            foreach ($subcommands as $subcommand => $details) {
+                Output::print(CliStrings::replace("\r\n  <green>$subcommand <cyan>".$details["args"]."<nc>\r\n"));
+                Output::print(CliStrings::get($details["desc"]),"\r\n");
+            }
+        }
+        $text = ob_get_flush();
+        file_put_contents(ROOT . "/welcome.txt",$text);
     }
 }
